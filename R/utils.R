8ce86e1bcba86c30de33ea53df8bae5961e95e73
Phil Donovan <philiprogerdonovan@gmail.com>
2018-04-23 09:58:38 +1200

    Added cleaning of long data. Still exprimental

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,60 @@ table_to_long <- function(data_set){
   else tidyr::gather(data_set, variable, value, -1, -2, -3)
 
 }
+
+clean_census_columns <- function(long_df){
+  #' Clean variables
+  #'
+  #' Cleans the variable names and splits them into columns in a long table.
+  #'
+  #' @param long_df
+  #' @examples
+  #' nz_dwelling_regions_long <- read_nz_census_data(dwelling_regions, include_gis = FALSE, long = TRUE, replace_confidential_values = NA_integer_)
+  #' cleaned_data <- clean_census_columns(nz_dwelling_regions_long)
+  #' ggplot(
+  #'   cleaned_data %>% filter(topic == "dwelling record type for occupied dwellings", variable == "Occupied Non-private Dwelling"),
+  #'   aes(x = year, y = value, colour = Description, group = Description)) +
+  #'   geom_line() +
+  #'   theme_bw()
+  #' @export
+  #' @importFrom stringr str_replace str_replace_all
+  #' @importFrom dplyr pull mutate select
+
+  # Extract variable columns
+  variables <- dplyr::pull(long_df, variable)
+
+  # Extract years
+  years <- stringr::str_extract(variables, "....")
+
+  # Extract "topics" and "variables"
+  census_topics_variables <- variables %>%
+    stringr::str_replace_all("_", " ") %>%
+    stringr::str_replace("20.. Census ", "") %>%
+    stringr::str_split(" ") %>%
+    lapply(split_topics)
+
+  census_topics_variables <- do.call("rbind", census_topics_variables) %>%
+    dplyr::mutate(year = years) %>%
+    dplyr::select(year, 1:3)
+
+  cleaned_data <- cbind(long_df %>% dplyr::select(-variable), census_topics_variables) %>%
+    dplyr::select(1:3, year, topic, variable, everything())
+
+  return(cleaned_data)
+
+
+}
+
+split_topics_variables <- function(topic_variable){
+  #' Split the topics and variables
+  #'
+  #' Splits the variavble column of the long data into years, topics and variables.
+  #' Only intended for use inside of clean_census_variables()
+  #' @importFrom stringr str_detect
+
+  topic <- topic_variable[!stringr::str_detect(topic_variable, "[A-Z]")] %>% paste(collapse = " ")
+  variable <- topic_variable[stringr::str_detect(topic_variable, "[A-Z]")] %>% paste(collapse = " ")
+
+  return(data.frame(topic = topic, variable = variable))
+
+  }
